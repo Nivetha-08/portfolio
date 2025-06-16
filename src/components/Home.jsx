7da61828c2160255261da76b6ffb315e33da03a4
Nivetha <nivethamaniofficial@gmail.com>
2025-06-16 14:38:30 +0530

    third commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 // import resume from '../../public/NivethaResume.pdf';
 
 const Home = () => {
+
+  function sendMain() {
+      const name = document.querySelector('.cname').value
+      const email = document.querySelector('.cmail').value
+      const message = document.querySelector('.cmsg').value
+
+      if(name && email && message ){
+        const mailtoLink = `mailto:mail2vasanthofficial@gmail.com?subject=Message from ${name}&body=Name: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
+        window.location.href = mailtoLink
+      }
+    }
   
   return (
     <>
@@ -54,7 +65,7 @@ const Home = () => {
                         </mask>
                         <g mask="url(#mask0)">
                             <path d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z"/>
-                            <image className="home__blob-img" x="50" y="60" href="assets/img/perfil.png"/>
+                            <image className="home__blob-img" x="50" y="60" href="assets/img/perfil (2).png"/>
                         </g>
                     </svg>
                 </div>
@@ -191,7 +202,14 @@ const Home = () => {
               <input type="text" placeholder="Name" className="contact__input" name="name" required/>
               <input type="email" placeholder="Email" className="contact__input" name="email" required/>
               <textarea cols="0" rows="10" className="contact__input" name="message" required></textarea>
-              <input type="submit" value="Send" className="contact__button button"/>
+              {/* <input type="submit" value="Send" className="contact__button button"/> */}
+              {/* <button className="btn btn-primary px-4" id="cbtn" onClick={sendMain} type="submit">Send</button> */}
+              <div className="d-flex justify-content-end">
+                <button className="btn btn-primary px-4" id="cbtn" onClick={sendMain} type="submit">
+                Send
+                </button>
+              </div>
+
             </form>
           </div>
         </section>
